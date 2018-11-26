de886b08ca1845c7443e7ec07efd6f01b029f77e
Ben <bendy557@gmail.com>
2018-11-26 19:17:06 +0000

    using c# udpclient

--- DrivingDemo/Assets/Scripts/Networking/NetworkTest.cs
+++ DrivingDemo/Assets/Scripts/Networking/NetworkTest.cs
@@ -16,15 +16,11 @@ class NetworkTest : MonoBehaviour
     //192.168.0.9 mac laptop
     //172.16.20.138 work desktop
 
-    private Socket m_sendSocket;
-    private Socket m_recieveSocket;
     private int port = 2349;
 
     private System.Net.IPEndPoint m_remoteEndPoint;
 
     string host;
-
-
 
     ////////////////////////
     private UdpClient m_udpClient;
@@ -42,72 +38,22 @@ class NetworkTest : MonoBehaviour
             Debug.Log(tempIP.AddressFamily.ToString() + " " + tempIP.ToString());
         }
 
-
-        //string tempIPAddr = 
-
-        //Debug.Log("My IP address: " + tempIPAddr)
-
-        //sending socket
-        m_sendSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
         System.Net.IPAddress ipAdd = System.Net.IPAddress.Parse(m_targetIPAddr);
         m_remoteEndPoint = new IPEndPoint(ipAdd, port);
-        //m_sendSocket.Connect(m_remoteEndPoint);
-        //m_sendSocket.
-
-        //RecievingSocket
-        m_recieveSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
-        m_recieveSocket.Blocking = false;
-        //System.Net.IPEndPoint localEndPoint = new IPEndPoint(m_localIPAddr, port);
-        System.Net.IPEndPoint localEndPoint = new IPEndPoint(IPAddress.Any, port);
-        m_recieveSocket.Bind(localEndPoint);
-
-        m_udpClient = new UdpClient(0);
+        m_udpClient = new UdpClient(port);
         m_udpClient.Client.Blocking = false;
-        //m_udpClient.Client.MulticastLoopback = true;
-        m_udpClient.Client.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.UseLoopback, true);
+        m_udpClient.Client.MulticastLoopback = true;
+        //m_udpClient.Client.SetSocketOption(SocketOptionLevel.Udp, SocketOptionName.UseLoopback, true);
     }
 
     private void OnDisable()
     {
         Debug.Log("Closed");
-        if (m_sendSocket != null)
-            m_sendSocket.Close();
-        if (m_recieveSocket != null)
-            m_recieveSocket.Close();
+        m_udpClient.Close();
     }
 
     private void Update()
     {
-        //And for reading back..
-        byte[] buffer = new byte[1024];
-        int iRx = -1;
-        char[] chars = new char[0];
-        try
-        {
-            iRx = m_recieveSocket.Receive(buffer);
-            chars = new char[iRx];
-
-            System.Text.Decoder d = System.Text.Encoding.UTF8.GetDecoder();
-            int charLen = d.GetChars(buffer, 0, iRx, chars, 0);
-            System.String recv = new System.String(chars);
-
-            Debug.Log(recv);
-        }
-        catch (SocketException e)
-        {
-            switch (e.SocketErrorCode)
-            {
-                case SocketError.WouldBlock:
-                    //WouldBlock
-                    break;
-                default:
-                    Debug.LogError("Exception not caught. Error code: " + e.SocketErrorCode + " " + (int)e.SocketErrorCode);
-                    break;
-            }
-        }
-
-
-
         try
         {
             IPEndPoint senderEndPoint = null;
@@ -134,17 +80,15 @@ class NetworkTest : MonoBehaviour
             return;
         }
 
-        for (int i = 0; i < 100; i++)
+        /*for (int i = 0; i < 100; i++)
         {
             //Start sending stuf..
             byte[] byData = System.Text.Encoding.ASCII.GetBytes("Message from: " + GetLocalIPAddress());
             m_sendSocket.SendTo(byData, m_remoteEndPoint);
-        }
+        }*/
 
-
-
-
-
+        byte[] dataSend = Encoding.ASCII.GetBytes("Shit");
+        m_udpClient.Send(dataSend, dataSend.Length, m_remoteEndPoint);
 
 
         Debug.Log("Message Sent");
