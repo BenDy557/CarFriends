53f8ddcd808efc664590ce2ffba27db161a82595
Ben <bendy557@gmail.com>
2019-05-02 19:47:34 +0100

    trying to get keyboard and gamepad ui input

--- DrivingDemo/Assets/Scripts/Managers/UIManager.cs
+++ DrivingDemo/Assets/Scripts/Managers/UIManager.cs
@@ -7,45 +7,15 @@ public class UIManager : MonoBehaviour
 {
     [SerializeField]
     private Canvas m_canvas;
-
     [SerializeField]
-    private Text m_speedText;
-
-    [SerializeField]
-    private WheelDisplay m_wheelDisplayPrefab;
-
-
-    [SerializeField]
-	private Vehicle m_vehicle;
+    private Button m_defaultButton;
+    
 
 
     private void Start()
     {
-        /*
-        //front left
-        WheelDisplay tempWheelDisplay = Instantiate(m_wheelDisplayPrefab, m_canvas.transform);
-        tempWheelDisplay.transform.localPosition = tempWheelDisplay.transform.localPosition + new Vector3(0f, 0f);
-        tempWheelDisplay.Wheel = m_vehicle.Engine.Wheels[0];
-        //front right
-        tempWheelDisplay = Instantiate(m_wheelDisplayPrefab, m_canvas.transform);
-        tempWheelDisplay.transform.localPosition = tempWheelDisplay.transform.localPosition + new Vector3(100f, 0f);
-		tempWheelDisplay.Wheel = m_vehicle.Engine.Wheels[1];
-
-        //back left
-        tempWheelDisplay = Instantiate(m_wheelDisplayPrefab, m_canvas.transform);
-        tempWheelDisplay.transform.localPosition = tempWheelDisplay.transform.localPosition + new Vector3(0f, -200f);
-		tempWheelDisplay.Wheel = m_vehicle.Engine.Wheels[2];
-        //back right
-        tempWheelDisplay = Instantiate(m_wheelDisplayPrefab, m_canvas.transform);
-        tempWheelDisplay.transform.localPosition = tempWheelDisplay.transform.localPosition + new Vector3(100f, -200f);
-		tempWheelDisplay.Wheel = m_vehicle.Engine.Wheels[3];
-        */
-
-        //Spawn wheel displays for each wheel
+        m_defaultButton.Select();
     }
 
-    private void Update()
-    {
-        m_speedText.text = "KPH: " + Mathf.RoundToInt((m_vehicle.Engine.Speed * 60f * 60f) * 0.001f);
-    }
+    
 }
